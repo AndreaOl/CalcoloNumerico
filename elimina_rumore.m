e6cc121b0cc768f914e48fd32e2892beacf8d3b7
AndreaOl <olandeseandrea@gmail.com>
2021-06-18 23:28:14 +0200

    eliminazione rumore

--- elimina_rumore.m
+++ elimina_rumore.m
@@ -1,26 +1,50 @@
-[audio,Fs] = audioread('Stayin Alive.mp3');
-audio = audio(:,1);
-n = length(audio);
+% Questo esempio elimina il rumore a frequenza elevata da una
+% registrazione.
+% Usage: play(player) per ascoltare il brano senza basso
+%        stop(player) per interrompere la riproduzione
 
-dt = 1/Fs;
+ 
+[audio_in,Fs] = audioread('Registrazione con rumore.wav');
+ 
+%df è il range minimo di frequenze
+n = length(audio_in);
+df = Fs/n;
+ 
+%Scegliamo i valori di frequenze che comporranno le ascisse del grafico
+frequenze = -Fs/2:df:Fs/2-df;
+ 
+%Applichiamo la trasformata di Fourier
+FFT_audio_in = fftshift(fft(audio_in)/length(fft(audio_in)));
+ 
+%Plotting della trasformata
+figure
+plot(frequenze,abs(FFT_audio_in));
+title("FFT dell'input audio");
+xlabel('Frequenze(HZ)');
+ylabel('Ampiezza');
+ 
+%Range di frequenze per isolare la registrazione dal rumore
+freq_taglio_inf = 150;
+freq_taglio_sup = 10000;
 
-%t = 0:dt:20-dt;
+%Tramite operazioni booleane applichiamo un filtro passa-banda 
+val = abs(frequenze)<freq_taglio_sup & abs(frequenze)>freq_taglio_inf;
 
-%plot(t, audio_in(1:length(t)))
+FFT_voc = FFT_audio_in(:,1);
+FFT_voc(~val) = 0;
 
-F = fft(audio);
-
-freq = (0:n/2)*Fs/n;
-
-freqq = (0:n-1)*Fs/n;
-H = ones(length(freq), 1);
-H(freqq < 2000) = 0;
-H(freqq > 40000) = 0;
-H(n/2+1:n)=fliplr(H(1:n/2));
-
-filtered = F.*H;
-
-plot(freq, abs(filtered(1:n/2+1)))
-
-player = audioplayer(ifft(filtered, n), Fs);
-play(player)
+%Plotting del segnale dopo l'applicazione del filtro
+figure
+plot(frequenze,abs(FFT_voc));
+title("FFT eliminando il rumore");
+xlabel('Frequenze(HZ)');
+ylabel('Ampiezza');
+ 
+%Applichiamo la trasformata inversa di Fourier
+FFT_a = ifftshift(FFT_voc);
+ 
+%Creiamo il segnale nel dominio del tempo
+s = ifft(FFT_a*length(fft(audio_in)));
+ 
+%Creiamo il player
+player = audioplayer(s3, Fs);
